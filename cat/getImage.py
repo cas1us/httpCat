8005a9fdbe0d6ac8d23ebb625fb90d5c4a049b4a
d9rect <d9rect@pm.me>
2021-11-26 00:50:10 +0000

    Removed download feature from module as we can use AsyncImage in kivy. Module now has 2 functions, one for removal of files and other for generating the cat. Main does not want to load image as it returns the function memory addr.

--- cat/getImage.py
+++ cat/getImage.py
@@ -1,12 +1,21 @@
 import random
-import requests
-import shutil
 import os
 import atexit
-import PIL.Image
 
 
 def run():
+    filesInDir = os.listdir(os.getcwd())
+    filesToDelte = [file for file in filesInDir if file.endswith(".jpg")]
+
+    def exitHandler():
+        for file in filesToDelte:
+            filePath = os.path.join(os.getcwd(), file)
+            os.remove(filePath)
+
+    atexit.register(exitHandler)
+
+
+def getCat():
     cats = [100, 101, 102, 200, 201, 202, 203, 204, 206, 207, 300, 301, 302, 303, 304, 305, 307, 308, 400, 401, 402,
             403,
             404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 420, 421, 422, 423, 424, 425,
@@ -15,34 +24,7 @@ def run():
             525,
             599]  # the list of cats
 
-    cat = random.choice(cats)  # get rand cat, split url
-    url = f'https://http.cat/{cat}.jpg'
-    filename = url.split("/")[-1]
-    os.chdir('cat')  # changes working directory in order to store images in /cat
-
-    r = requests.get(url, stream=True)
-    if r.status_code == 200:  # if we can download successfully, we save the image
-        r.raw.decode_content = True
-        with open(filename, 'wb') as f:
-            shutil.copyfileobj(r.raw, f)
-        print(f"Successfully downloaded image {filename}")
-    else:
-        print("Could not download image")
-
-    filesInDir = os.listdir(os.getcwd())
-    filesToDelte = [file for file in filesInDir if file.endswith(".jpg")]
-
-    def readsize():  # I assumed the images were not all the same size, but I am leaving this in
-        global imgWidth, imgHeight
-        img = PIL.Image.open(f'{cat}.jpg')
-        imgWidth, imgHeight = img.size
-        img.close()
-
-    readsize()  # read image size
-
-    def exitHandler():
-        for file in filesToDelte:
-            filePath = os.path.join(os.getcwd(), file)
-            os.remove(filePath)
-
-    atexit.register(exitHandler)
+    os.chdir('cat')
+    cat = random.choice(cats)
+    print(cat)
+    return cat
--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRaisedButton
+from kivy.uix.image import AsyncImage
 from cat import getImage
 
 sys.path.insert(0, '/cat')
 
 
 class Window(MDApp):
+    def press(self, instance):
+        getImage.run()
+
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.title = "httpCat"
@@ -18,8 +22,9 @@ class Window(MDApp):
         button = MDRaisedButton(text="Button", pos_hint={"center_x": .5,
                                                          "center_y": .8},
                                 md_bg_color=(1, 1, 1, 1),
-                                text_color=(0.0001, 0, 0, 1)
+                                text_color=(0.0001, 0, 0, 1),
                                 )
+        button.bind(on_press=self.press)
 
         label = MDLabel(text="Suh", pos_hint={'center_x': 0.5,
                                               'center_y': 0.5},
@@ -28,16 +33,19 @@ class Window(MDApp):
                         font_style="H1",
                         halign="center")
 
+        imgNumber = AsyncImage(source=f'http.cat/{getImage.getCat()}')
+        print(getImage.getCat)
+        print(imgNumber)
+
         screen.add_widget(button)
         screen.add_widget(label)
+        screen.add_widget(imgNumber)
         return screen
-
-    getImage.run()
 
 
 if __name__ == '__main__':
+    getImage.run()
     Window().run()
 
-# TODO: Delete tempfile when user wants new cat
 # TODO: Make DB storing how many times a user landed on specific cat and display it
 # TODO: Visualise data from SQL into bargraph/chart
